56eec76000283d638c99fcfbc7e6344b5fc364a9
Usmanasim11 <131867634+Usmanasim11@users.noreply.github.com>
2023-10-06 20:20:05 +0500

    Update index.js

--- nodeApplication/index.js
+++ nodeApplication/index.js
@@ -7,7 +7,7 @@ var server = http.createServer(function(request, response) {
 
 });
 
-var port = 4000;
+var port = 6000;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
